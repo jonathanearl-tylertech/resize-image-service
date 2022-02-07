8bf2cdee29f35ffbbaa0b7055c5c0b62d208825d
Jonathan Earl <jonathan.earl@tylertech.com>
2022-02-07 11:20:43 -0800

    use static methods on image transform class

--- lib/trasform-image.ts
+++ lib/trasform-image.ts
@@ -1,12 +1,12 @@
 import sharp from 'sharp';
 
 class TransformImage {
-    async toJpg(file: Buffer, height: number, width: number) {
+    public static async toJpg(file: Buffer, height: number, width: number) {
         let jpg = await sharp(file).resize(width, height).toFormat('jpg').toBuffer();
         return jpg;
     }
 
-    async toPng(file: Buffer, height: number, width: number) {
+    public static async toPng(file: Buffer, height: number, width: number) {
         let png = await sharp(file).resize(width, height).toFormat('png').toBuffer();
         return png;
     }
