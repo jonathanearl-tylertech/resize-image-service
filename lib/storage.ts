259521dc4c809f4929403d4307c7eca21235d73c
Jonathan Earl <jonathan.earl@tylertech.com>
2022-02-07 11:20:28 -0800

    add storage config interface

--- lib/storage.ts
+++ lib/storage.ts
@@ -1,14 +1,23 @@
 import S3, { ClientConfiguration } from 'aws-sdk/clients/s3';
 
+export interface StorageConfig {
+    accessKeyId: string,        // 'minioadmin'
+    secretAccessKey: string,    // 'minioadmin'
+    endpoint: string,           // 'http:127.0.0.1:9000'
+    s3ForcePathStyle: true,     // true
+    sslEnabled: boolean,        // false
+    signatureVersion: string    // 'v4'
+}
+
 class Storage {
-    public config: ClientConfiguration;
+    public config: StorageConfig;
     public bucket: string;
     public s3: S3;
 
-    constructor(config: ClientConfiguration, bucket: string) {
+    constructor(config: StorageConfig, bucket: string) {
         this.config = config;
         this.bucket = bucket;
-        this.s3 = new S3(this.config);
+        this.s3 = new S3(this.config as ClientConfiguration);
     }
 
     async getFile(key: string): Promise<Buffer> {
